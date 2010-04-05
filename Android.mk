4f2621523cbdb0e92d73c3e5b25815cc3d354b24
Brett Chabot <brettchabot@android.com>
2010-04-05 16:16:08 -0700

    Adjust DownloadProvider's makefile so its tests get built.
    Bug 2562721
    
    Change-Id: I7dfd73d4e30fcc5dc80c9ed024432c0b833a011a

--- Android.mk
+++ Android.mk
@@ -9,3 +9,6 @@ LOCAL_PACKAGE_NAME := DownloadProvider
 LOCAL_CERTIFICATE := media
 
 include $(BUILD_PACKAGE)
+
+# additionally, call tests makefiles
+include $(call all-makefiles-under,$(LOCAL_PATH))

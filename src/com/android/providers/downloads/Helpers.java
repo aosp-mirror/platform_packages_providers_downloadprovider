aosp-mirror/platform_packages_providers_downloadprovider

commit 38f1711910715538d6b64e505fe8fb9a0cd40475

when using DM public API, allow duplicate downloads of same file
bug:3132790
this mimics the behavior that exists today (in non-public API)

Change-Id: I722b3bc23fe8c9b0bf66a088c5f6a3130be2129f

src/com/android/providers/downloads/Helpers.java
@@ -99,25 +99,22 @@ public class Helpers {
             boolean isPublicApi) throws GenerateSaveFileError {
         checkCanHandleDownload(context, mimeType, destination, isPublicApi);
         if (destination == Downloads.Impl.DESTINATION_FILE_URI) {
-            return getPathForFileUri(hint, contentLength);
+            String path = verifyFileUri(hint, contentLength);
+            String c = getFullPath(path, mimeType, destination, null);
+            return c;
         } else {
             return chooseFullPath(context, url, hint, contentDisposition, contentLocation, mimeType,
                     destination, contentLength);
         }
     }
 
-    private static String getPathForFileUri(String hint, long contentLength)
+    private static String verifyFileUri(String hint, long contentLength)
             throws GenerateSaveFileError {
         if (!isExternalMediaMounted()) {
             throw new GenerateSaveFileError(Downloads.Impl.STATUS_DEVICE_NOT_FOUND_ERROR,
                     "external media not mounted");
         }
         String path = Uri.parse(hint).getPath();
-        if (new File(path).exists()) {
-            Log.d(Constants.TAG, "File already exists: " + path);
-            throw new GenerateSaveFileError(Downloads.Impl.STATUS_FILE_ALREADY_EXISTS_ERROR,
-                    "requested destination file already exists");
-        }
         if (getAvailableBytes(getFilesystemRoot(path)) < contentLength) {
             throw new GenerateSaveFileError(Downloads.Impl.STATUS_INSUFFICIENT_SPACE_ERROR,
                     "insufficient space on external storage");
@@ -148,11 +145,15 @@ public class Helpers {
         File base = locateDestinationDirectory(context, mimeType, destination, contentLength);
         String filename = chooseFilename(url, hint, contentDisposition, contentLocation,
                                          destination);
+        return getFullPath(filename, mimeType, destination, base);
+    }
 
+    private static String getFullPath(String filename, String mimeType, int destination,
+        File base) throws GenerateSaveFileError {
         // Split filename between base and extension
         // Add an extension if filename does not have one
         String extension = null;
-        int dotIndex = filename.indexOf('.');
+        int dotIndex = filename.lastIndexOf('.');
         if (dotIndex < 0) {
             extension = chooseExtensionFromMimeType(mimeType, true);
         } else {
@@ -162,12 +163,13 @@ public class Helpers {
 
         boolean recoveryDir = Constants.RECOVERY_DIRECTORY.equalsIgnoreCase(filename + extension);
 
-        filename = base.getPath() + File.separator + filename;
+        if (base != null) {
+            filename = base.getPath() + File.separator + filename;
+        }
 
         if (Constants.LOGVV) {
             Log.v(Constants.TAG, "target file: " + filename + extension);
         }
-
         return chooseUniqueFilename(destination, filename, extension, recoveryDir);
     }
 

commit c21507f025f7422429975442792a6c7e279378da

Don't change extensions for explicitly set download locations.
This regression from GB was introduced by 38f1711910715538d6b64e505fe8fb9a0cd40475,
which was intended to allow duplicate downloads of the same file, adding -<n> to
the end of file names.  As a side effect, this also activated extension validation
logic, which adds/changes an extension to match the Mimetype.

This change keeps the unique filename logic but prevents extension changes when an
explicit filename is set.  Thus, it is still possible for the actual download
location to differ from the requested one, but only if the file already exists.

Bug: 5196436
Change-Id: I198dc2a819c5d839a05b72c25e0830d889a9c5a3

src/com/android/providers/downloads/Helpers.java
@@ -97,18 +97,28 @@ public class Helpers {
         return path;
     }
 
-    static String getFullPath(String filename, String mimeType, int destination,
-        File base) throws StopRequestException {
-        // Split filename between base and extension
-        // Add an extension if filename does not have one
+    static String getFullPath(String filename, String mimeType, int destination, File base)
+            throws StopRequestException {
         String extension = null;
         int dotIndex = filename.lastIndexOf('.');
         boolean missingExtension = dotIndex < 0 || dotIndex < filename.lastIndexOf('/');
-        if (missingExtension) {
-            extension = chooseExtensionFromMimeType(mimeType, true);
+        if (destination == Downloads.Impl.DESTINATION_FILE_URI) {
+            // Destination is explicitly set - do not change the extension
+            if (missingExtension) {
+                extension = "";
+            } else {
+                extension = filename.substring(dotIndex);
+                filename = filename.substring(0, dotIndex);
+            }
         } else {
-            extension = chooseExtensionFromFilename(mimeType, destination, filename, dotIndex);
-            filename = filename.substring(0, dotIndex);
+            // Split filename between base and extension
+            // Add an extension if filename does not have one
+            if (missingExtension) {
+                extension = chooseExtensionFromMimeType(mimeType, true);
+            } else {
+                extension = chooseExtensionFromFilename(mimeType, destination, filename, dotIndex);
+                filename = filename.substring(0, dotIndex);
+            }
         }
 
         boolean recoveryDir = Constants.RECOVERY_DIRECTORY.equalsIgnoreCase(filename + extension);
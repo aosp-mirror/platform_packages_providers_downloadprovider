8ec005728d47e282d72801b4bb43c9a3ea8f717f
Jeff Sharkey <jsharkey@android.com>
2016-01-07 14:15:59 -0700

    Use resolved path for both checking and opening.
    This avoids a race condition where someone can change a symlink
    target after the security checks have passed.
    
    Bug: 26211054
    Change-Id: I5842aaecc7b7d417a3b1902957b59b8a1f3c1ccb

--- src/com/android/providers/downloads/DownloadProvider.java
+++ src/com/android/providers/downloads/DownloadProvider.java
@@ -1260,9 +1260,15 @@ public final class DownloadProvider extends ContentProvider {
             throw new FileNotFoundException("No filename found.");
         }
 
-        final File file = new File(path);
+        final File file;
+        try {
+            file = new File(path).getCanonicalFile();
+        } catch (IOException e) {
+            throw new FileNotFoundException(e.getMessage());
+        }
+
         if (!Helpers.isFilenameValid(getContext(), file)) {
-            throw new FileNotFoundException("Invalid file: " + file);
+            throw new FileNotFoundException("Invalid file path: " + file);
         }
 
         final int pfdMode = ParcelFileDescriptor.parseMode(mode);
